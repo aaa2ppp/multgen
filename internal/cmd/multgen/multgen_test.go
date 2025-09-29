99263885901275be833caa934a7cfef410e6b37d
Aleksandr Panfilov <aaa2ppp@gmail.com>
2025-09-29 14:06:14 +0300

    fix(bench): keep-alive mode of fast client and std/fast benchmarks unification

--- internal/cmd/multgen/multgen_test.go
+++ internal/cmd/multgen/multgen_test.go
@@ -8,57 +8,91 @@ import (
 	"time"
 
 	"github.com/aaa2ppp/be"
+	"github.com/valyala/fasthttp"
+
 	fastapi "github.com/aaa2ppp/multgen/internal/api/fast"
 	api "github.com/aaa2ppp/multgen/internal/api/std"
 	"github.com/aaa2ppp/multgen/internal/solver"
 	"github.com/aaa2ppp/multgen/internal/testutils"
-	"github.com/valyala/fasthttp"
+)
+
+const (
+	withKeepAlive    = "with keep alive"
+	withoutKeepAlive = "without keep alive"
 )
 
 func BenchmarkHTTPServer(b *testing.B) {
-	b.Run("single", func(b *testing.B) {
-		for _, prefix := range []string{"without", "with"} {
-			b.Run(prefix+" keep alive", func(b *testing.B) {
-				url, close := startHTTPServer(b)
-				defer close()
+	benchs := []struct {
+		name        string
+		startServer func(*testing.B) (addr string, stopServer func())
+	}{
+		{
+			"std",
+			startHTTPServer,
+		},
+		{
+			// TODO: Investigate source of 1 alloc/op (25B) — probably fasthttp.Client or Solver internals.
+			"fast",
+			startFastHTTPServer,
+		},
+	}
 
-				client := newFastHTTPClient(prefix == "with")
+	for _, tb := range benchs {
+		b.Run(tb.name, func(b *testing.B) {
 
-				b.ResetTimer()
-				b.ReportAllocs()
+			b.Run("single", func(b *testing.B) {
+				for _, name := range []string{withoutKeepAlive, withKeepAlive} {
+					b.Run(name, func(b *testing.B) {
+						addr, stopServer := tb.startServer(b)
+						defer stopServer()
 
-				testutils.AddRPSMetricToBenchmark(b, func() {
-					for i := 0; i < b.N; i++ {
-						doGetFast(b, client, url)
-					}
-				})
-			})
-		}
-	})
+						url := "http://" + addr + "/get"
 
-	b.Run("parallel", func(b *testing.B) {
-		for _, prefix := range []string{"without", "with"} {
-			b.Run(prefix+" keep alive", func(b *testing.B) {
-				url, close := startHTTPServer(b)
-				defer close()
+						// always use a fast client to reduce the pressure on the benchmark
+						client := newFastHTTPClient(b, name == withKeepAlive)
 
-				b.ResetTimer()
-				b.ReportAllocs()
+						b.ResetTimer()
+						b.ReportAllocs()
 
-				testutils.AddRPSMetricToBenchmark(b, func() {
-					b.RunParallel(func(pb *testing.PB) {
-						client := newFastHTTPClient(prefix == "with")
-						for pb.Next() {
-							doGetFast(b, client, url)
-						}
+						testutils.AddRPSMetricToBenchmark(b, func() {
+							for i := 0; i < b.N; i++ {
+								client.Get(url)
+							}
+						})
 					})
-				})
+				}
 			})
-		}
-	})
+
+			b.Run("parallel", func(b *testing.B) {
+				for _, name := range []string{withoutKeepAlive, withKeepAlive} {
+					b.Run(name, func(b *testing.B) {
+						addr, stopServer := tb.startServer(b)
+						defer stopServer()
+
+						url := "http://" + addr + "/get"
+
+						b.ResetTimer()
+						b.ReportAllocs()
+
+						testutils.AddRPSMetricToBenchmark(b, func() {
+							b.RunParallel(func(pb *testing.PB) {
+
+								// always use a fast client to reduce the pressure on the benchmark
+								client := newFastHTTPClient(b, name == withKeepAlive)
+
+								for pb.Next() {
+									client.Get(url)
+								}
+							})
+						})
+					})
+				}
+			})
+		})
+	}
 }
 
-func startHTTPServer(b *testing.B) (url string, closeServer func()) {
+func startHTTPServer(b *testing.B) (addr string, stopServer func()) {
 	s, err := solver.New(solver.DefaultConfig())
 	be.Err(b, err, nil)
 
@@ -73,81 +107,37 @@ func startHTTPServer(b *testing.B) (url string, closeServer func()) {
 		_ = server.Serve(listener)
 	}()
 
-	addr := listener.Addr().String()
+	addr = listener.Addr().String()
 
-	url = "http://" + addr + "/get"
-	closeServer = func() { server.Close() }
-
-	return url, closeServer
+	return addr, func() { server.Close() }
 }
 
-func newHTTPClient(keepAlive bool) *http.Client {
-	return &http.Client{
-		Transport: &http.Transport{
-			DisableKeepAlives:   !keepAlive,
-			MaxIdleConns:        100,
-			MaxIdleConnsPerHost: 100,
-			IdleConnTimeout:     90 * time.Second,
+type httpClient struct {
+	b      *testing.B
+	client http.Client
+}
+
+func newHTTPClient(b *testing.B, keepAlive bool) *httpClient {
+	return &httpClient{
+		b: b,
+		client: http.Client{
+			Transport: &http.Transport{
+				DisableKeepAlives:   !keepAlive,
+				MaxIdleConns:        100,
+				MaxIdleConnsPerHost: 100,
+				IdleConnTimeout:     90 * time.Second,
+			},
+			Timeout: 5 * time.Second,
 		},
-		Timeout: 5 * time.Second,
 	}
 }
-
-func doGet(b *testing.B, client *http.Client, url string) {
-	resp, err := client.Get(url)
-	_ = err
-	// be.Err(b, err, nil)
+func (c *httpClient) Get(url string) {
+	resp, _ := c.client.Get(url)
 	_, _ = io.Copy(io.Discard, resp.Body)
 	_ = resp.Body.Close()
 }
 
-func BenchmarkFastHTTPServer(b *testing.B) {
-
-	// TODO: Investigate source of 1 alloc/op (25B) — probably fasthttp.Client internals.
-
-	b.Run("single", func(b *testing.B) {
-		for _, prefix := range []string{"without", "with"} {
-			b.Run(prefix+" keep alive", func(b *testing.B) {
-				url, close := startFastHTTPServer(b)
-				defer close()
-
-				client := newFastHTTPClient(prefix == "with")
-
-				b.ResetTimer()
-				b.ReportAllocs()
-
-				testutils.AddRPSMetricToBenchmark(b, func() {
-					for i := 0; i < b.N; i++ {
-						doGetFast(b, client, url)
-					}
-				})
-			})
-		}
-	})
-
-	b.Run("parallel", func(b *testing.B) {
-		for _, prefix := range []string{"without", "with"} {
-			b.Run(prefix+" keep alive", func(b *testing.B) {
-				url, close := startFastHTTPServer(b)
-				defer close()
-
-				b.ResetTimer()
-				b.ReportAllocs()
-
-				testutils.AddRPSMetricToBenchmark(b, func() {
-					b.RunParallel(func(pb *testing.PB) {
-						client := newFastHTTPClient(prefix == "with")
-						for pb.Next() {
-							doGetFast(b, client, url)
-						}
-					})
-				})
-			})
-		}
-	})
-}
-
-func startFastHTTPServer(b *testing.B) (url string, closeServer func()) {
+func startFastHTTPServer(b *testing.B) (addr string, closeServer func()) {
 	s, err := solver.New(solver.DefaultConfig())
 	be.Err(b, err, nil)
 	handler := fastapi.New(s)
@@ -161,28 +151,35 @@ func startFastHTTPServer(b *testing.B) (url string, closeServer func()) {
 		_ = fasthttp.Serve(listener, handler)
 	}()
 
-	addr := listener.Addr().String()
-	url = "http://" + addr + "/get"
+	addr = listener.Addr().String()
 
-	closeServer = func() {
+	return addr, func() {
 		listener.Close()
 		<-serverClosed
 	}
-
-	return url, closeServer
 }
 
-func newFastHTTPClient(keepAlive bool) *fasthttp.Client {
-	return &fasthttp.Client{
-		MaxConnsPerHost:               100,
-		MaxIdleConnDuration:           90 * time.Second,
-		DisableHeaderNamesNormalizing: true,
-		ReadTimeout:                   5 * time.Second,
-		WriteTimeout:                  5 * time.Second,
+type fastHTTPClient struct {
+	b         *testing.B
+	client    fasthttp.Client
+	keepAlive bool
+}
+
+func newFastHTTPClient(b *testing.B, keepAlive bool) *fastHTTPClient {
+	return &fastHTTPClient{
+		b: b,
+		client: fasthttp.Client{
+			MaxConnsPerHost:               100,
+			MaxIdleConnDuration:           90 * time.Second,
+			DisableHeaderNamesNormalizing: true,
+			ReadTimeout:                   5 * time.Second,
+			WriteTimeout:                  5 * time.Second,
+		},
+		keepAlive: keepAlive,
 	}
 }
 
-func doGetFast(b *testing.B, client *fasthttp.Client, url string) {
+func (c *fastHTTPClient) Get(url string) {
 	req := fasthttp.AcquireRequest()
 	resp := fasthttp.AcquireResponse()
 	defer fasthttp.ReleaseRequest(req)
@@ -191,6 +188,10 @@ func doGetFast(b *testing.B, client *fasthttp.Client, url string) {
 	req.SetRequestURI(url)
 	req.Header.SetMethod("GET")
 
-	_ = client.Do(req, resp)
+	if !c.keepAlive {
+		req.Header.Set("Connection", "close")
+	}
+
+	_ = c.client.Do(req, resp)
 	// тело уже в resp.Body()
 }
